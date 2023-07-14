73f4c10a1922605ad34b594e59b9511d88f0548e
Adil Hussain <>
2023-07-14 12:10:50 +0100

    Added a couple of library modules.

---
+++ library2/src/main/java/com/tazkiyatech/experiments/library/HelloWorld.kt
@@ -0,0 +1,12 @@
+package com.tazkiyatech.experiments.library
+
+/**
+ * A container of super useful functions to help you say "Hello" in your applications.
+ */
+class HelloWorld {
+
+    /**
+     * @return The String `"Hello"`.
+     */
+    fun sayHello() = "Hello"
+}
